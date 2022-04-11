d0461ae13497ed3f36eaffc118710f4f402b4684
aliccciao <65285479+aliccciao@users.noreply.github.com>
2022-04-11 09:27:40 +0700

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan bilangan bulat pertama: ");
+        int bil1 = sc.nextInt();
+        System.out.print("Masukkan bilangan bulat kedua: ");
+        int bil2 = sc.nextInt();
+        sc.close();
+
+        if (bil2 > bil1) {
+            int container = bil1;
+            bil1 = bil2;
+            bil2 = container;
+        }
+
+        int sisaPembagi = bil1 - ((bil1 / bil2)*bil2);
+        while (sisaPembagi != 0) {
+            System.out.printf("%d = %d * %d + %d\n", bil1, bil1/bil2, bil2, sisaPembagi);
+            bil1 = bil2;
+            bil2 = sisaPembagi;
+            sisaPembagi = bil1 - ((bil1 / bil2)*bil2);
+        }
+        System.out.printf("%d = %d * %d + %d\n", bil1, bil1/bil2, bil2, sisaPembagi);
+    }
+}
